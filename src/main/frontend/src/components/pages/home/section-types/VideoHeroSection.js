c55da80b59cf2b9c9822239b6bf879f92487f42c
bkrmalick <bkrmalick@hotmail.com>
2020-11-13 23:15:24 +0000

    issue with components folder being duplicated on git

---
+++ src/main/frontend/src/components/pages/home/section-types/VideoHeroSection.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Button } from '../../../Button';
+
+import { FaInfoCircle } from 'react-icons/fa';
+
+import './VideoHeroSection.css';
+
+function VideoHeroSection() {
+    //NEED TO GIVE CREDS TO: <a rel="nofollow" href="http://www.videezy.com/">B Roll by Videezy!</a>
+
+    //https://www.videezy.com/abstract/18793-virus-cells-viruses-virus-cells-under-microscope-floating-in-fluid-with-blue-background
+
+    //TODO add parallax effect
+    //TODO Not displaying on mobile properly
+    return (
+        <div className="hero-container">
+            <video src="videos/video-2.mp4" autoPlay playsInline loop muted /> 
+            <h1 className="fade-in">STAY INFORMED, STAY SAFE</h1> 
+            <p className="fade-in">Track recent COVID-19 cases in London</p>
+            <div className="hero-btns">
+                <Link to="/go" >
+                        <Button className="btns" buttonStyle="btn--outline" buttonSize="btn--large">GO</Button>
+                </Link>
+
+                <Link to="/about" >
+                    <Button className="btns" buttonStyle="btn--primary" buttonSize="btn--large"><FaInfoCircle className="navbar-icon" /> MORE INFO</Button>
+                </Link>
+            </div>
+        </div>
+    );
+}
+
+export default VideoHeroSection
